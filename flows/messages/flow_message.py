bc2cfc18aba31e880c1f92319a76e654704dab13
martinj96 <martin.josifoski@epfl.ch>
2023-06-27 01:33:36 -0700

    Update the flow message formats.

--- flows/messages/flow_message.py
+++ flows/messages/flow_message.py
@@ -1,45 +1,149 @@
 from dataclasses import dataclass
-from typing import List, Any, Union
-import copy
+from typing import List, Any, Dict, Optional
+import colorama
 
 from flows.messages import Message
 
-
-@dataclass
-class TaskMessage(Message):
-    expected_outputs: List[str]
-    target_flow_run_id: str
-    task_name: str
-
-    def __init__(self, **kwargs):
-        super(TaskMessage, self).__init__(**kwargs)
-        self.expected_outputs = kwargs.pop("expected_outputs", [])
-        self.target_flow_run_id = kwargs.pop("target_flow_run_id", None)
-        self.task_name = kwargs.pop("task_name", "")
-
-    def __repr__(self):
-        extended = copy.deepcopy(self.data)
-        extended["expected_outputs"] = self.expected_outputs
-        return repr(extended)
+colorama.init()
 
 
 @dataclass
-class StateUpdateMessage(Message):
-    updated_keys: List[str]
+class InputMessage(Message):
+    def __init__(self,
+                 src_flow: str,
+                 dst_flow: str,
+                 expected_outputs: List[str],
+                 api_keys: Optional[Dict[str, str]] = None,
+                 keys_to_ignore_for_hash: Optional[List[str]] = None,
+                 **kwargs):
+        super().__init__(**kwargs)
 
-    def __init__(self, **kwargs):
-        super(StateUpdateMessage, self).__init__(**kwargs)
-        self.updated_keys = kwargs.pop("updated_keys", {})
+        self.data["src_flow"] = src_flow
+        self.data["dst_flow"] = dst_flow
+        self.data["expected_outputs"] = expected_outputs
+        if api_keys:
+            self.data["api_keys"] = api_keys
+
+        # ~~~ Initialize keys to ignore for hash ~~~
+        self.keys_to_ignore_for_hash = []
+        if keys_to_ignore_for_hash:
+            self.keys_to_ignore_for_hash = keys_to_ignore_for_hash
+        if "api_keys" not in self.keys_to_ignore_for_hash:
+            self.keys_to_ignore_for_hash.append("api_keys")
+
+    def to_string(self):
+        src_flow = self.data["src_flow"]
+        dst_flow = self.data["dst_flow"]
+
+        message = f"\n{colorama.Fore.GREEN} ~~~ InputMessage: `{src_flow}` --> `{dst_flow}` ~~~\n" \
+                  f"{colorama.Fore.WHITE}{self.__str__()}{colorama.Style.RESET_ALL}"
+
+        return message
+
+
+@dataclass
+class UpdateMessage_Generic(Message):
+    def __init__(self,
+                 updated_flow: str,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.data["updated_flow"] = updated_flow
+
+    def to_string(self):
+        updated_flow = self.data["updated_flow"]
+
+        message = f"\n{colorama.Fore.MAGENTA} ~~~ UpdateMessage ({self.__class__.__name__}): `{updated_flow}` ~~~\n" \
+                  f"{colorama.Fore.WHITE}{self.__str__()}{colorama.Style.RESET_ALL}"
+
+        return message
+
+
+@dataclass
+class UpdateMessage_ChatMessage(UpdateMessage_Generic):
+    def __init__(self,
+                 content: str,
+                 role: str,
+                 updated_flow: str,
+                 **kwargs):
+
+        super().__init__(updated_flow=updated_flow, **kwargs)
+        self.data["role"] = role
+        self.data["content"] = content
+
+    def to_string(self):
+        updated_flow = self.data["updated_flow"]
+        role = self.data["role"]
+        color = colorama.Fore.RED if role == "assistant" else colorama.Fore.YELLOW
+
+        message = f"\n{color} ~~~ UpdateMessage ({self.__class__.__name__}): " \
+                  f"`{updated_flow}` (role: {role}) ~~~\n" \
+                  f"{colorama.Fore.WHITE}{self.__str__()}{colorama.Style.RESET_ALL}"
+
+        return message
+
+
+@dataclass
+class UpdateMessage_NamespaceReset(Message):
+    def __init__(self,
+                 updated_flow: str,
+                 created_by: str,
+                 keys_deleted_from_namespace: List[str]):
+        super().__init__(created_by=created_by, data={})
+        self.data["updated_flow"] = updated_flow
+        self.data["keys_deleted_from_namespace"] = keys_deleted_from_namespace
+
+    def to_string(self):
+        updated_flow = self.data["updated_flow"]
+
+        message = f"\n{colorama.Fore.CYAN} ~~~ ResetMessageNamespaceOnly ({self.__class__.__name__}): `{updated_flow}` ~~~\n" \
+                  f"{colorama.Fore.WHITE}{self.__str__()}{colorama.Style.RESET_ALL}"
+
+        return message
+
+
+@dataclass
+class UpdateMessage_FullReset(Message):
+    def __init__(self,
+                 updated_flow: str,
+                 created_by: str,
+                 keys_deleted_from_namespace: List[str]):
+        super().__init__(created_by=created_by, data={})
+        self.data["updated_flow"] = updated_flow
+        self.data["keys_deleted_from_namespace"] = keys_deleted_from_namespace
+
+    def to_string(self):
+        updated_flow = self.data["updated_flow"]
+
+        message = f"\n{colorama.Fore.CYAN} ~~~ ResetMessageFull ({self.__class__.__name__}): `{updated_flow}` ~~~\n" \
+                  f"{colorama.Fore.WHITE}{self.__str__()}{colorama.Style.RESET_ALL}"
+
+        return message
 
 
 @dataclass
 class OutputMessage(Message):
-    error_message: Union[None, str]
-    history: Any
+    def __init__(self,
+                 src_flow: str,
+                 dst_flow: str,
+                 expected_outputs: List[str],
+                 outputs: Dict[str, Any],
+                 missing_expected_outputs: List[str],
+                 history: 'FlowHistory',
+                 **kwargs):
+        super().__init__(**kwargs)
 
-    def __init__(self, **kwargs):
-        super(OutputMessage, self).__init__(**kwargs)
-        self.error_message = kwargs.pop("error_message", None)
+        self.data["src_flow"] = src_flow
+        self.data["dst_flow"] = dst_flow
+        self.data["expected_outputs"] = expected_outputs
+        self.data["outputs"] = outputs
+        self.data["missing_expected_outputs"] = missing_expected_outputs
+        self.history = history.to_list()
 
-        from flows.history import FlowHistory
-        self.history: FlowHistory = kwargs.pop("history", FlowHistory())
+    def to_string(self):
+        src_flow = self.data["src_flow"]
+        dst_flow = self.data["dst_flow"]
+
+        message = f"\n{colorama.Fore.BLUE} ~~~ OutputMessage: `{src_flow}` --> `{dst_flow}` ~~~\n" \
+                  f"{colorama.Fore.WHITE}{self.__str__()}{colorama.Style.RESET_ALL}"
+
+        return message
