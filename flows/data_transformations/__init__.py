99474dae765ec8e934bb25cb2ac60249ca23ca20
martinj96 <martin.josifoski@epfl.ch>
2023-07-05 14:43:19 -0700

    Add KeyCopy data transformation.

--- flows/data_transformations/__init__.py
+++ flows/data_transformations/__init__.py
@@ -1,3 +1,4 @@
 from .key_rename import KeyRename
+from .key_copy import KeyCopy
 from .end_of_interaction import EndOfInteraction
 from .regex_extractor_first import RegexFirstOccurrenceExtractor
---
+++ flows/data_transformations/key_copy.py
@@ -0,0 +1,25 @@
+from typing import Dict, Any
+
+from flows.data_transformations import KeyRename
+from flows.utils.general_helpers import flatten_dict, unflatten_dict
+
+
+
+class KeyCopy(KeyRename):
+    def __init__(self,
+                 old_key2new_key: Dict[str, str],
+                 flatten_data_dict: bool = False):
+        super().__init__(old_key2new_key, flatten_data_dict)
+
+    def __call__(self, data_dict: Dict[str, Any], **kwargs) -> Dict[str, Any]:
+        if self.flatten_data_dict:
+            data_dict = flatten_dict(data_dict)
+
+        for old_key, new_key in self.old_key2new_key.items():
+            if old_key in data_dict:
+                data_dict[new_key] = data_dict[old_key]
+
+        if self.flatten_data_dict:
+            data_dict = unflatten_dict(data_dict)
+
+        return data_dict
