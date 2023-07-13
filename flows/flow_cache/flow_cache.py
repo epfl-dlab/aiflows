33bae0699f64bd5841e86f28a7294ef4772151c0
yeeef <lyeeef@gmail.com>
2023-07-13 15:53:46 +0800

    cache refactor - deprecate flow_run_cache - remove caching_utils - add flow_cache.Flow_Cache

---
+++ flows/flow_cache/flow_cache.py
@@ -0,0 +1,92 @@
+import functools
+import os
+import hashlib
+import threading
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Any
+import copy
+from diskcache import Index
+from ..utils import logging
+
+log = logging.get_logger(__name__)
+
+
+@dataclass
+class CACHING_PARAMETERS:
+    # ToDo(https://github.com/epfl-dlab/flows/issues/66): Expose these params in the launcher (and the configs when available)
+    #   The params should be updated before the library is loaded. Make sure to do this reliably.
+    #   How to do this in a clean way?
+    # ToDo: Add support for infinite cache if we intend to use it for resuming/extending runs.
+    #   Is that even a good idea?
+    #   What happens if you want to resume/extend a run that has been already resumed/extended?
+    # Global parameters that can be set before starting the outer-flow
+    max_cached_entries: int = 1000
+    do_caching: bool = True
+    cache_dir: str = None
+
+CACHING_PARAMETERS.do_caching = os.getenv("FLOW_DISABLE_CACHE", "false").lower() == "false"
+
+@dataclass
+class CachingValue:
+    output_results: Dict
+    full_state: Dict
+    history_messages_created: List
+
+@dataclass
+class CachingKey:
+    flow: str
+    input_data: Dict[str, Any]
+    keys_to_ignore_for_hash: List[str]
+
+    def hash_string(self) -> str:
+        return _custom_hash(self.flow, self.input_data, self.keys_to_ignore_for_hash)
+
+def get_cache_dir() -> str:
+    cache_dir = CACHING_PARAMETERS.cache_dir
+    if cache_dir:
+        return os.path.join(cache_dir)
+
+    current_dir = os.getcwd()
+    return os.path.abspath(os.path.join(current_dir, '.flow_cache'))
+
+
+def _custom_hash(*all_args):
+    _repr_args = []
+    for arg in all_args:
+        _repr_args.append(repr(arg))
+    return hashlib.sha256(repr(_repr_args).encode("utf-8")).hexdigest()
+
+
+class FlowCache:
+    def __init__(self):
+        self._index = Index(get_cache_dir())
+        # TODO(yeeef): why do we need the lock? from https://grantjenks.com/docs/diskcache/tutorial.html#index, Index can be even used to do inter-process / inter-thread communication
+        self.__lock = threading.Lock()
+
+    def get(self, key: CachingKey) -> Optional[CachingValue]:
+        key = key.hash_string()
+
+        with self.__lock:
+            return self._index.get(key, None)
+    
+    def set(self, key: CachingKey, value: CachingValue):
+        key = key.hash_string()
+
+        with self.__lock:
+            self._index[key] = value
+
+    def pop(self, key: CachingKey):
+        key = _custom_hash(key.flow, key.input_data, key.keys_to_ignore_for_hash)
+
+        with self.__lock:
+            return self._index.pop(key)
+    
+    def __len__(self):
+        with self.__lock:
+            return len(self._index)    
+
+
+def clear_cache():
+    cache_dir = get_cache_dir()
+    cache = Index(cache_dir)
+    cache.clear()
--- tests/flow_cache/test_flow_cache.py
+++ tests/flow_cache/test_flow_cache.py
@@ -1,9 +1,10 @@
-import pytest
+import os
 import mock
+import pytest
 from typing import Any, Dict, List, Optional, MutableMapping
 
 from flows.base_flows import AtomicFlow
-from flows.utils.caching_utils import flow_run_cache, clear_cache, CACHING_PARAMETERS
+from flows.flow_cache import clear_cache, CACHING_PARAMETERS, FlowCache
 from flows.utils.general_helpers import flatten_dict
 from flows.data_transformations import KeyRename
 from flows.utils.logging import set_verbosity_debug
@@ -12,59 +13,24 @@ from flows.utils.logging import set_verbosity_debug
 class EchoFlow(AtomicFlow):
     SUPPORTS_CACHING = True
 
-    # TODO(yeeef): why do we need this decorator at all? just fancier?
-    # what if I just want to use fixed_reply flow? but I have to inherit it
-    # and add `flow_run_cache` explicitly?
-    # and `flow_run_cache` + `SUPPORTS_CACHING` is just redundant information, it only
-    # makes sense if someone inherits something?
-
-    # and this decorator just makes things puzzled. Because some parameters are for cache..
-
-    #     REQUIRED_KEYS_CONFIG = ["name", "description", "clear_flow_namespace_on_run_end", "keep_raw_response"]
-    # REQUIRED_KEYS_CONSTRUCTOR = ["flow_config", "input_data_transformations", "output_data_transformations"]
-
-
-    # TODO(yeeef): too many requirements for a flow...
-    #              input_data_transformations and output_data_transformations is confused because it is not in the REQUIRED_KEYS_CONFIG
-    @flow_run_cache() 
     def run(self,
-            *,
-            input_data: Dict[str, Any],
-            private_keys: Optional[List[str]] = None,
-            keys_to_ignore_for_hash: Optional[List[str]] = None,
-            enable_cache: bool = False) -> Dict[str, Any]:
+            input_data: Dict[str, Any]) -> Dict[str, Any]:
         
         user_input = input_data["user_input"]
         return {"echo": user_input}
 
-class EchoFlow2(EchoFlow):
+class EchoFlowNoCache(EchoFlow):
     SUPPORTS_CACHING = False
 
-cache_data = {}
+    def run(self,
+            input_data: Dict[str, Any]) -> Dict[str, Any]:
+        return super().run(input_data=input_data)
 
-class MockCacheIndex(MutableMapping):
-    def __init__(self, _):
-        self._data = cache_data
-
-    def __getitem__(self, key):
-        return self._data[key]
-
-    def __setitem__(self, key, value):
-        self._data[key] = value
-
-    def __delitem__(self, key):
-        del self._data[key]
-
-    def __iter__(self):
-        return iter(self._data)
-
-    def __len__(self):
-        return len(self._data)
-
-@pytest.fixture(scope="module", autouse=True)
-def mock_cache_index():
-    with mock.patch("flows.utils.caching_utils.Index", new=MockCacheIndex) as mock_sync:
-        yield mock_sync
+@pytest.fixture(autouse=True)
+def reset_cache():
+    clear_cache()
+    yield None
+    clear_cache()
 
 @pytest.fixture
 def enable_cache_globally():
@@ -74,69 +40,77 @@ def enable_cache_globally():
 def disable_cache_globally():
     CACHING_PARAMETERS.do_caching = False
 
-@pytest.fixture(autouse=True)
-def clear_cache_before_test():
-    cache_data.clear()
-
 def test_enable_cache_globally(enable_cache_globally):
-    # TODO(Yeeef): instantiate_from_config exposes too many details
-
-    assert len(cache_data) == 0
+    # set_verbosity_debug()
+    
     echo_flow = EchoFlow.instantiate_from_default_config({
         "name": "EchoFlow",
         "description": "EchoFlow",
         "input_keys": ["user_input"],
-        # "output_keys": ["echo"],
+        "enable_cache": True
     })
+    assert(len(echo_flow.cache)) == 0
+
     # print(echo_flow)
     input_message = echo_flow.package_input_message(
         {"user_input": "hello"}
     )
     output_message = echo_flow(input_message)
     # print(output_message)
-    assert len(cache_data) > 0
+    assert len(echo_flow.cache) > 0
 
 def test_enable_cache_globally_disable_flow_wise(enable_cache_globally):
-    # set_verbosity_debug()
-    CACHING_PARAMETERS.do_caching = False
-
-    assert len(cache_data) == 0
     echo_flow = EchoFlow.instantiate_from_default_config({
         "name": "EchoFlow",
         "description": "EchoFlow",
-        "input_keys": ["user_input"],
-        # "output_keys": ["echo"],
+        "enable_cache": False
     })
+    assert len(echo_flow.cache) == 0
 
     input_message = echo_flow.package_input_message(
         {"user_input": "hello"}
     )
-    output_message = echo_flow(input_message, enable_cache=False)
+    output_message = echo_flow(input_message)
 
-    assert len(cache_data) == 0
+    assert len(echo_flow.cache) == 0
+
+def test_enable_cache_globally_flow_not_support_cache(enable_cache_globally):
+    echo_flow = EchoFlowNoCache.instantiate_from_default_config({
+        "name": "EchoFlow",
+        "description": "EchoFlow",
+        "enable_cache": True
+    })
+    assert len(echo_flow.cache) == 0
+
+    input_message = echo_flow.package_input_message(
+        {"user_input": "hello"}
+    )
+    try:
+        output_message = echo_flow(input_message)
+    except:
+        assert len(echo_flow.cache) == 0
+        return
+    
+    assert False, "Should have raised an exception"
+
 
 
 def test_disable_cache_globally(disable_cache_globally):
-    # set_verbosity_debug()
-    CACHING_PARAMETERS.do_caching = False
 
-    assert len(cache_data) == 0
     echo_flow = EchoFlow.instantiate_from_default_config({
         "name": "EchoFlow",
         "description": "EchoFlow",
         "input_keys": ["user_input"],
+        "enable_cache": True
         # "output_keys": ["echo"],
     })
+    assert len(echo_flow.cache) == 0
     # print(echo_flow)
     input_message = echo_flow.package_input_message(
         {"user_input": "hello"}
     )
     output_message = echo_flow(input_message)
 
-    input_message = echo_flow.package_input_message(
-        {"user_input": "hello"}
-    )
-    output_message = echo_flow(input_message, enable_cache=True)
     # print(output_message)
-    assert len(cache_data) == 0
+    assert len(echo_flow.cache) == 0
 
