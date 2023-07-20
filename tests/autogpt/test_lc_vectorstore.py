2a428d572f1b0d1ec07ba8dc4f0846cfe57f2563
3Represents <jiheng.wei@outlook.com>
2023-07-20 15:11:10 +0200

    Rename module in test

--- tests/autogpt/test_lc_vectorstore.py
+++ tests/autogpt/test_lc_vectorstore.py
@@ -1,5 +1,5 @@
 import os
-from flows.application_flows.VectorStoreFlow import VectorStoreFlow
+from flows.application_flows.VectorStoreModule import VectorStoreFlow
 
 def test_vec():
     mem_flow = VectorStoreFlow.instantiate_from_default_config({
