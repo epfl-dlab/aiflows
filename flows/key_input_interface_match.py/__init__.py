fc024cccadb71d6695776b0585fbf2710feaeea1
martinj96 <martin.josifoski@epfl.ch>
2023-08-03 13:06:08 -0700

    Remove some files.

--- flows/key_input_interface_match.py/__init__.py
+++
@@ -1,9 +0,0 @@
-from .key_rename import KeyRename
-from .key_copy import KeyCopy
-from .key_set import KeySet
-from .key_select import KeySelect
-from .key_delete import KeyDelete
-from .end_of_interaction import EndOfInteraction
-from .regex_extractor_first import RegexFirstOccurrenceExtractor
-from .json import Json2Obj, Obj2Json
-from .unnesting_dict import UnNesting
