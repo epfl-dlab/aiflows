c72434f23ba1bc1affe847261f0960883f838273
martinj96 <martin.josifoski@epfl.ch>
2023-11-05 21:29:04 +0100

    Remove the unused (and obsolete) LCChainFlow.

--- flows/application_flows/LCChainFlowModule/__init__.py
+++
@@ -1,1 +0,0 @@
-from .LCChainFlow import LCChainFlow
--- flows/application_flows/__init__.py
+++
@@ -1,9 +0,0 @@
-# from .OpenAIFlowModule import OpenAIChatAtomicFlow
-# from .VectorStoreFlowModule import ChromaDBFlow
-# from .LCToolFlowModule import LCToolFlow
-# from .HumanInputFlowModule import HumanInputFlow
-# from .LCVectorStoreFlowModule import LCVectorStoreFlow
-# from .OpenAIChatHumanFlowModule import OpenAIChatHumanFlowModule
-# from .FixedReplyFlowModule.fixed_reply import FixedReplyFlow
-# from .GeneratorCriticFlowModule.generator_critic import GeneratorCriticFlow
-from .LCChainFlowModule import LCChainFlow
