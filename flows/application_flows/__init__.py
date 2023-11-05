f6860e861bcc03a8beb4ff36d86ba75458ca6fa6
martinj96 <martin.josifoski@epfl.ch>
2023-11-05 21:28:14 +0100

    Move out the rest of the application flow and remove unused code.

--- flows/application_flows/__init__.py
+++ flows/application_flows/__init__.py
@@ -2,8 +2,8 @@
 # from .VectorStoreFlowModule import ChromaDBFlow
 # from .LCToolFlowModule import LCToolFlow
 # from .HumanInputFlowModule import HumanInputFlow
-from .LCChainFlowModule import LCChainFlow
 # from .LCVectorStoreFlowModule import LCVectorStoreFlow
-from .OpenAIChatHumanFlowModule import OpenAIChatHumanFlowModule
-from .GeneratorCriticFlowModule.generator_critic import GeneratorCriticFlow
-from .FixedReplyFlowModule.fixed_reply import FixedReplyFlow
+# from .OpenAIChatHumanFlowModule import OpenAIChatHumanFlowModule
+# from .FixedReplyFlowModule.fixed_reply import FixedReplyFlow
+# from .GeneratorCriticFlowModule.generator_critic import GeneratorCriticFlow
+from .LCChainFlowModule import LCChainFlow
