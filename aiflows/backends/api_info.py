db3aa277c55b321793d9ceb4da02a6f354a3469a
Nicky <nicolas.mario.baldwin@gmail.com>
2024-03-26 11:16:49 +0100

    error msg api info

--- aiflows/backends/api_info.py
+++ aiflows/backends/api_info.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from typing import Optional
-
-
+from aiflows.utils import logging
+logger = logging.get_logger(__name__)
+logger.warn = logger.warning
 @dataclass
 class ApiInfo:
     """This class contains the information about an API key.
@@ -20,3 +21,17 @@ class ApiInfo:
     api_version: Optional[str] = None
     api_key: str = None
     api_base: Optional[str] = None
+    
+    def __post_init__(self):
+        self.validate()
+
+    def validate(self):
+        if self.backend_used is None:
+            logger.warn("backend_used is set to None")
+            
+        if self.api_key is None:
+            logger.warn(" Your api_key is set to None. If this is not intended, "
+                        "please set it to a valid key (Note: if you've loaded your key from an environment variable, "
+                        "consider checking if the environment variable is set correctly "
+                        "and reactivating the environment variable.)")
+        
